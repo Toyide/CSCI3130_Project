152a19c2dae4094ec53659559cac02da0601bfd9
toyide <yd907592@dal.ca>
2018-02-20 20:45:48 -0400

    Initial commit

---
+++ app/src/main/java/com/toyide/csci3130_project/NavActivity.java
@@ -0,0 +1,54 @@
+package com.toyide.csci3130_project;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class NavActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch (item.getItemId()) {
+                case R.id.navigation_profile:
+                    transaction.replace(R.id.MainFrame,new ProfileFragment()).commit();
+                    return true;
+                case R.id.navigation_schedule:
+                    transaction.replace(R.id.MainFrame,new ScheduleFragment()).commit();
+                    return true;
+                case R.id.navigation_register:
+                    transaction.replace(R.id.MainFrame,new RegisterFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.MainFrame,new ProfileFragment()).commit();
+    }
+
+}
