0cad67b8e5c656050dfbd72442281d77f9cdb24d
Liwei <menliweisammy@gmail.com>
2018-02-28 08:56:05 +0800

    Test

--- app/src/main/java/com/toyide/csci3130_project/ProfileFragment.java
+++ app/src/main/java/com/toyide/csci3130_project/ProfileFragment.java
@@ -1,13 +1,19 @@
 package com.toyide.csci3130_project;
 
+import android.app.Activity;
 import android.content.Context;
+
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+
+import android.widget.*;
+
 
 
 /**
@@ -18,11 +24,26 @@ import android.widget.Toast;
  * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+
+public class ProfileFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    EditText oldPass;
+    EditText newPass;
+    TextView name;
+    TextView info1;
+    TextView state1;
+    TextView state2;
+    TextView state3;
+    TextView pass1;
+    TextView pass2;
+    Button state;
+    Button info;
+    Button pass;
+    Button modify;
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -55,21 +76,113 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
+
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view= inflater.inflate(R.layout.fragment_profile, container,false);
+
+        name = view.findViewById(R.id.viewAntoNieva);
+        info1 = view.findViewById(R.id.textView);
+        state1 = view.findViewById(R.id.textView2);
+        state2 = view.findViewById(R.id.textView3);
+        state3 = view.findViewById(R.id.textView4);
+        pass1 = view.findViewById(R.id.textView5);
+        pass2 = view.findViewById(R.id.textView6);
+        oldPass = view.findViewById(R.id.editText2);
+        modify = view.findViewById(R.id.button3);
+        state = view.findViewById(R.id.state);
+        info =view.findViewById(R.id.info);
+        pass = view.findViewById(R.id.pass);
+        modify = view.findViewById(R.id.confirm);
+        state.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menonClick(v);
+            }
+        });
+        info.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                menonClick(v);
+            }
+        });
+        pass.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                menonClick(v);
+            }
+        });
+        return view;
+
+
+    }
+    public void menonClick(View v) {
+        switch (v.getId()) {
+            case R.id.info: {
+                state1.setVisibility(View.INVISIBLE);
+                state2.setVisibility(View.INVISIBLE);
+                state3.setVisibility(View.INVISIBLE);
+                pass1.setVisibility(View.INVISIBLE);
+                pass2.setVisibility(View.INVISIBLE);
+                modify.setVisibility(View.INVISIBLE);
+                info1.setVisibility(View.VISIBLE);
+                oldPass.setVisibility(View.INVISIBLE);
+                newPass.setVisibility(View.INVISIBLE);
+                modify.setVisibility((View.INVISIBLE));
+                break;
+            }
+            case R.id.state: {
+
+                info1.setVisibility(View.INVISIBLE);
+                state1.setVisibility(View.VISIBLE);
+                state2.setVisibility(View.VISIBLE);
+                state3.setVisibility(View.VISIBLE);
+                pass1.setVisibility(View.INVISIBLE);
+                pass2.setVisibility(View.INVISIBLE);
+                oldPass.setVisibility(View.INVISIBLE);
+                newPass.setVisibility(View.INVISIBLE);
+                modify.setVisibility((View.INVISIBLE));
+                break;
+            }
+            case R.id.pass: {
+                info1.setVisibility(View.INVISIBLE);
+                state1.setVisibility(View.INVISIBLE);
+                state2.setVisibility(View.INVISIBLE);
+                state3.setVisibility(View.INVISIBLE);
+                pass1.setVisibility(View.VISIBLE);
+                pass2.setVisibility(View.VISIBLE);
+                oldPass.setVisibility(View.VISIBLE);
+                newPass.setVisibility(View.VISIBLE);
+                modify.setVisibility((View.VISIBLE));
+                break;
+            }
+            default:
+                break;
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
+
+
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
@@ -89,6 +202,11 @@ public class ProfileFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        menonClick(v);
     }
 
     /**
