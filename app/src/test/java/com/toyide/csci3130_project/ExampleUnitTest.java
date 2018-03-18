f8978d97e30e509913b513edd0bcb20d34daef13
Ayarol <whatandwhich@gmail.com>
2018-03-18 17:33:04 -0300

    fixed name issue in Profile.java

--- app/src/test/java/com/toyide/csci3130_project/ExampleUnitTest.java
+++ app/src/test/java/com/toyide/csci3130_project/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.toyide.csci3130_project;
 
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,9 +13,22 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     checkPassword check = new checkPassword();
+    Profile profile = new Profile("B00123456", "name","12345678", "Computer Science", "undergraduate");
+    Map<String, Object>  check2 =profile.toMap();
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public  void check_profile_map() throws Exception {
+        assertEquals("B00123456", check2.get("UserID").toString() );
+        assertEquals("name", check2.get("UserName").toString());
+        assertEquals("12345678",check2.get("Password").toString());
+        assertEquals("Computer Science", check2.get("Department").toString());
+        assertEquals("undergraduate", check2.get("UserDegree").toString());
+
+    }
+
 
 }
