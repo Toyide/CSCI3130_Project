ca2decb9adc5d45bd007665b8530ae0859ce2258
jyyang5 <jyyang55@outlook.com>
2018-03-31 17:07:33 -0300

    <RegisterInfoDisplayAndUI><JingyunYang_YideGe>Proper display of courseName with modified UI

--- app/src/main/java/com/toyide/csci3130_project/RegistrationAdapter.java
+++ app/src/main/java/com/toyide/csci3130_project/RegistrationAdapter.java
@@ -1,6 +1,7 @@
 package com.toyide.csci3130_project;
 
 import android.content.Context;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -33,17 +34,35 @@ public class RegistrationAdapter extends ArrayAdapter<Courses> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.registration_content, parent, false);
         }
 
-        TextView courseTitleView = (TextView) convertView.findViewById(R.id.courseName);
-        TextView courseInfoView = (TextView) convertView.findViewById(R.id.courseInfo);
+        TextView courseTitleView = (TextView) convertView.findViewById(R.id.courseTitle);
+        final TextView civ = (TextView) convertView.findViewById(R.id.courseInfo);
         TextView courseTime = (TextView) convertView.findViewById(R.id.courseTime);
         TextView courseSpot = (TextView) convertView.findViewById(R.id.courseSpot);
         CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkBox);
         Log.i(TAG, "MyClass.getView()  " + course.CourseInfo+" secod");
         //set text for TextView
-        courseTitleView.setText(course.CourseTitle);
-        //courseInfoView.setText(course.CourseInfo);
-        courseTime.setText(course.CourseWeekday + course.CourseTime);
-        courseSpot.setText(course.SpotCurrent + "/" + course.SpotMax);
+        courseTitleView.setText(" "+course.CourseTitle.substring(0,3)+ "\n"+course.CourseTitle.substring(4));
+        civ.setText(" "+course.CourseInfo);
+        //civ.setSingleLine(true);
+        civ.setMovementMethod(ScrollingMovementMethod.getInstance());
+        //Collapse if not long enough to display
+        civ.setOnClickListener(new View.OnClickListener() {
+            Boolean flag = true;
+            @Override
+            public void onClick(View v) {
+                Log.i("civ.getLineCount()",civ.getLineCount()+"");
+                if(flag){
+                    flag = false;
+                    civ.setEllipsize(null);
+
+                }
+
+            }
+        });
+
+        courseTime.setText(" "+course.CourseWeekday + "\n"+ course.CourseTime);
+        courseSpot.setText(" "+course.SpotCurrent + "/" + course.SpotMax);
+
 
 
         return convertView;
